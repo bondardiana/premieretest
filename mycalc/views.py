5f9d0434843fe625093e823a413fd618dd8472eb
bondardiana <bondar110701@gmail.com>
2023-07-13 18:08:31 +0200

    deserts added

--- mycalc/views.py
+++ mycalc/views.py
@@ -24,29 +24,44 @@ datas = {
         '1': ['Салати', 'Салат Цезар з куркою та беконом',  240, 'salad_cesar.jpg'],
         '2': ['Салати', 'Салат Грецький',  155, 'salad_greek.jpg'],
         '3': ['Салати', 'Теплий Салат з філе міньйон',270, 'salad_cesar.jpg'],
-        '4': ['Салати', 'Салат з лососем та авокадо',  360, 'salad_cesar.jpg'],
-        '5': ['Салати', 'Салат з курячою печінкою', 165, 'salad_cesar.jpg'],
-        '6': ['Салати', "Салат олів'є з телятиною", 165, 'salad_cesar.jpg'],
+        '4': ['Салати', 'Салат з лососем та авокадо',  360, 'salad_minion.jpg'],
+        '5': ['Салати', 'Салат з курячою печінкою', 165, 'salad_pechin.jpg'],
+        '6': ['Салати', "Салат олів'є з телятиною", 165, 'salad_olivie.jpg'],
         '7': ['Салати', "Салат з капустою, огірком та редисом",55, 'salad_cabage.jpg'],
-        '8': ['Супи', "Солонка м'ясна", 175, 'salad_cesar.jpg'],
-        '9': ['Супи', "Окрошка з телятиною", 185, 'salad_cesar.jpg'],
+        '8': ['Супи', "Солонка м'ясна", 175, 'soup_solonka.jpg'],
+        '9': ['Супи', "Окрошка з телятиною", 185, 'soup_okroshka.jpg'],
         '10': ['Супи', "Борщ з яловичими ребрами", 185, 'soup_borsch.jpg'],
-        '11': ['Супи', "Крем-суп грибний",  125, 'salad_cesar.jpg'],
-        '12': ['Супи', "Бульйон з домашньою лапшою та яйцем", 115, 'soup_lapsha.jpg']}
+        '11': ['Супи', "Крем-суп грибний",  125, 'soup_mashrooms.jpg'],
+        '12': ['Супи', "Бульйон з домашньою лапшою та яйцем", 115, 'soup_lapsha.jpg'],
+        '13': ['Десерти', "Сирники в полуничному соусі з мигдалевими пластівцями", 145, 'deserts_cheesecake.jpg'],
+        '14': ['Десерти', "Чізкейк з ягідними кулями", 135, 'deserts_cheesecake2.jpg'],
+        '15': ['Десерти', "Тірамісу", 125, 'deserts_tiramisu.jpg']
+}
 
 
 def generate_pdf(all_data):
-
     mytable = [['Категорія', 'Блюдо', 'Ціна', 'Кількість', 'Всього']]
+    total_q = 0
+    dishes = []
     for i, e in enumerate(all_data):
         #e = element.cleaned_data['element']
-        print('lol')
-        print(i, e)
         n = e['input_data']
         if n != 0:
             r = datas[str(i + 1)]
-            row = [r[0], r[1], r[2], n, int(n)*int(r[2])]
+            sum = int(n)*int(r[2])
+            total_q += sum
+            row = [r[0], r[1], r[2], n, sum]
             mytable.append(row)
+
+            # Images pasting
+            imagename = 'images/' + r[-1]
+            image = Image(imagename)
+            image.drawHeight = 130
+            image.drawWidth = 130
+            dishes.append(image)
+
+    row = ["", "", "", "Загалом", total_q]
+    mytable.append(row)
 
     style = TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.whitesmoke),
@@ -54,8 +69,9 @@ def generate_pdf(all_data):
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
         ('FONTNAME', (0, 0), (-1, -1), 'Adine_Kirnberg_Regular'),
         ('FONTSIZE', (0, 0), (-1, -1), 22),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-        ('GRID', (0, 0), (-1, -1), 1, colors.grey),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.Color(0.914, 0.871, 0.804, alpha=0.5)),
+        ('LINEBELOW', (0, 0), (-1, -2), 1, colors.grey),  # Add horizontal gridline below header row
+        ('BACKGROUND', (0, -1), (-1, -1), colors.white),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 20),
     ])
 
@@ -64,14 +80,37 @@ def generate_pdf(all_data):
 
     filename = "table_example.pdf"
     pdf = SimpleDocTemplate(filename, pagesize=letter)
-    pdf.build([table])
+
+    #Adding Logo Image
+    image = Image('images/logo.jpg')
+    image.drawHeight = 100
+    image.drawWidth = 170
+    image.hAlign = 'LEFT'
+
+    spacer = Spacer(1, 50)
+
+    style_images = [
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),  # Align images to the top of cells
+    ]
+    colWidths, rowHeights = 150, 150
+    if len(dishes) > 4:
+        dishes_table = Table([dishes[0:4], dishes[4:]], colWidths=colWidths, rowHeights=rowHeights)
+    else:
+        dishes_table = Table([dishes], colWidths=colWidths, rowHeights=rowHeights)
+    dishes_table.setStyle(style_images)
+
+
+    #PDF BUILD
+    results = [image, spacer, table, spacer, dishes_table]
+    pdf.build(results)
+
     return filename
 
 
 def calculation(request):
     all_data = []
     if request.method == 'POST':
-        for i in range(0, 13):
+        for i in range(0, 16):
             form = Calculation(request.POST, prefix=f'form{i}')
             if form.is_valid():
                 data = form.cleaned_data  # Get the cleaned data from the form
